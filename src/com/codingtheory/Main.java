a88c4c2bcbd0958460174b6748ae11939c84edaa
fbisquerra <fbisquerra@travelcompositor.com>
2020-03-19 17:33:28 +0100

    Problem
    Repetition of the AuditTrail code in both tasks and no strong way to determine how to use the AuditTrail object in any of the clases

--- src/com/codingtheory/Main.java
+++ src/com/codingtheory/Main.java
@@ -1,19 +1,9 @@
 package com.codingtheory;
 
-import com.codingtheory.strategy.BlackAndWhiteFilter;
-import com.codingtheory.strategy.ImageStorage;
-import com.codingtheory.strategy.JpegCompressor;
-import com.codingtheory.strategy.exercice.ChatClient;
-import com.codingtheory.strategy.exercice.DESEncryptor;
-
 public class Main {
 
     public static void main(String[] args) {
 
-       var imageStorage = new ImageStorage(new JpegCompressor(), new BlackAndWhiteFilter());
-       imageStorage.store("file name");
 
-       var chat = new ChatClient(new DESEncryptor());
-       chat.send("message");
     }
 }
---
+++ src/com/codingtheory/template/AuditTrail.java
@@ -0,0 +1,8 @@
+package com.codingtheory.template;
+
+public class AuditTrail {
+
+    public void record() {
+        System.out.println("Audit");
+    }
+}
