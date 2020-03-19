d54191411a382189a62e10726decfe4e7a488729
fbisquerra <fbisquerra@travelcompositor.com>
2020-03-19 09:57:36 +0100

    Add a new implementation

--- src/com/codingtheory/Main.java
+++ src/com/codingtheory/Main.java
@@ -1,14 +1,14 @@
 package com.codingtheory;
 
-import com.codingtheory.state.BrushTool;
 import com.codingtheory.state.Canvas;
+import com.codingtheory.state.EraserTool;
 
 public class Main {
 
     public static void main(String[] args) {
 
         var canvas = new Canvas();
-        canvas.setCurrentTool(new BrushTool());
+        canvas.setCurrentTool(new EraserTool());
         canvas.mouseDown();
         canvas.mouseUp();
     }
